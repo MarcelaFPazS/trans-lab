3f7269d33972220d206d2e9c9729515161969409
Marcela Paz <marcela.fpaz.s@gmail.com>
2018-06-27 13:14:55 -0400

    actuañizacion de trabajo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 /* Holiii acá va tu código también */
+const go = () => { //solo funciona con este nombre y contraseña
+  if (document.form.password.value <=8 ){ 
+    window.open('menu.html') //abre página index si se pone correcto
+    } 
+    else{ 
+      alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
+  } 
+  
+  
